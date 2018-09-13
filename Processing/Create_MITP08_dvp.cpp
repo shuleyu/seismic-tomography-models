416d9a83a896fa7b4e5ea7c4b84bd47a580ffa40
Shule Yu <ysl6.626@gmail.com>
2018-09-13 13:33:23 -0700

    MITP08

---
+++ Processing/Create_MITP08_dvp.cpp
@@ -0,0 +1,140 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<cmath>
+#include<set>
+#include<algorithm>
+
+extern "C" {
+// Current version: netcdf-4.6.1
+#include<netcdf.h>
+}
+
+// the downloaded file is renamed as "MITP08.txt";
+
+using namespace std;
+
+int main(){
+    
+    vector<vector<float>> raw_data;
+    float lat,lon,depth,dvp;
+    set<float> s_lat,s_lon,s_depth;
+    string tmpstr;
+    ifstream fpin("MITP08.txt");
+
+    getline(fpin,tmpstr);
+    while (fpin >> lat >> lon >> depth >> dvp){
+        raw_data.push_back({depth,lat,lon,dvp});
+        s_lat.insert(lat);
+        s_lon.insert(lon);
+        s_depth.insert(depth);
+    }
+    fpin.close();
+
+    auto cmp=[](const vector<float> &v1, const vector<float> &v2){
+        if (v1[0]==v2[0]) {
+            if (v1[1]==v2[1]) return v1[2]<v2[2];
+            else return v1[1]<v2[1];
+        }
+        else return v1[0]<v2[0];
+    };
+    sort(raw_data.begin(),raw_data.end(),cmp);
+
+    // write nc file.
+    int retval,ncid;
+    string outfile="MITP08_dvp.nc";
+
+    retval=nc_create(outfile.c_str(),NC_CLOBBER,&ncid);
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+
+
+    // define dimension id.
+    int depth_dimid,lat_dimid,lon_dimid;
+    retval=nc_def_dim(ncid,"depth",s_depth.size(),&depth_dimid);
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+    retval=nc_def_dim(ncid,"latitude",s_lat.size(),&lat_dimid);
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+    retval=nc_def_dim(ncid,"longitude",s_lon.size(),&lon_dimid);
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+
+    // v is not a dimension, just a variable,
+    // we will generate a variable id from the dimension ids from dimensions.
+    int dimids[3];
+    dimids[0]=depth_dimid;
+    dimids[1]=lat_dimid;
+    dimids[2]=lon_dimid;
+
+
+    // define variable id.
+    int depth_varid,lat_varid,lon_varid;
+    retval=nc_def_var(ncid,"depth",NC_FLOAT,1,&depth_dimid,&depth_varid);
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+    retval=nc_def_var(ncid,"latitude",NC_FLOAT,1,&lat_dimid,&lat_varid);
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+    retval=nc_def_var(ncid,"longitude",NC_FLOAT,1,&lon_dimid,&lon_varid);
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+
+    int v_varid;
+    retval=nc_def_var(ncid,"v",NC_FLOAT,3,dimids,&v_varid);
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+
+    // define units.
+    string unit_depth="km_downward",unit_lat="degrees_north",unit_lon="degrees_east";
+    retval=nc_put_att_text(ncid,depth_varid,"units",unit_depth.size(),unit_depth.c_str());
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+    retval=nc_put_att_text(ncid,lat_varid,"units",unit_lat.size(),unit_lat.c_str());
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+    retval=nc_put_att_text(ncid,lon_varid,"units",unit_lon.size(),unit_lon.c_str());
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+
+    string unit_v="dvp, (%), relative to ak135";
+    retval=nc_put_att_text(ncid,v_varid,"units",unit_v.size(),unit_v.c_str());
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+
+    // End of variable definition.
+    retval=nc_enddef(ncid);
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+
+    // load data.
+
+    float *data;
+    size_t i;
+
+    /// depth.
+    data=new float [s_depth.size()];
+    i=0;
+    for (auto &item:s_depth) data[i++]=item;
+    retval=nc_put_var_float(ncid,depth_varid,data);
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+    delete [] data;
+
+    /// lat.
+    data=new float [s_lat.size()];
+    i=0;
+    for (auto &item:s_lat) data[i++]=item;
+    retval=nc_put_var_float(ncid,lat_varid,data);
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+    delete [] data;
+
+    /// lon.
+    data=new float [s_lon.size()];
+    i=0;
+    for (auto &item:s_lon) data[i++]=item;
+    retval=nc_put_var_float(ncid,lon_varid,data);
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+    delete [] data;
+
+    /// v.
+    data=new float [raw_data.size()];
+    i=0;
+    for (auto &item:raw_data) data[i++]=item[3];
+    retval=nc_put_var_float(ncid,v_varid,data);
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+    delete [] data;
+
+    // close the nc file.
+    retval=nc_close(ncid);
+    if (retval!=0) throw std::runtime_error(nc_strerror(retval));
+    
+    return 0;
+}
--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 In this database, I collected several whole mantle tomography results, and
 converted them into nc files. Each nc files contain 4 variables: depth,
 longitude, latitude and "v". The "v"alues could represent either S or P wave
-speed (in km/s), density (in g/cm^3), Q or anisotropic parameters, etc. The
-details(comments) are within each nc files, but the file name roughly
-indicate which value it represent.
+speed (in km/s), velocity purturbation (%), density (in kg/m^3), Q or
+anisotropic parameters, etc. The details(comments) are within each nc files,
+but the file name roughly indicate which value it represent.
 
 Most of the model are downloaded from IRIS SPUD Service:\
 http://ds.iris.edu/spud/earthmodel\
@@ -18,22 +18,32 @@ https://github.com/shuleyu/CPP-Library/blob/master/Examples/Tomography.cpp
 
 Here are details about download sources and convertions to these nc files:
 
-1. GyPSum_S.nc\
+1. GyPSum_vs.nc\
 Paper        : doi:10.1029/2010JB007631\
 Download link: http://ds.iris.edu/spud/earthmodel/10131119 (netCDF binary of S velocity expressed as km/s)\
 Changes      : rename variable "vs" to "v".
 
-2. GyPSum_P.nc\
+2. GyPSum_vp.nc\
 Paper        : doi:10.1029/2010JB007631\
 Download link: http://ds.iris.edu/spud/earthmodel/10131119 (netCDF binary of P velocity expressed as km/s)\
 Changes      : rename variable "vp" to "v".
 
-3. SEMum_S.nc\
+3. SEMum_vs.nc\
 Paper        : doi:10.1111/j.1365-246X.2011.04969.x\
 Download link: http://ds.iris.edu/spud/earthmodel/10131263 (the netCDF binary for the above model expressed as absolute velocity)\
 Changes      : lose variable "xi", rename variable "vs" to "v".
 
-4. SEMum_XI.nc\
+4. SEMum_xi.nc\
 Paper        : doi:10.1111/j.1365-246X.2011.04969.x\
 Download link: http://ds.iris.edu/spud/earthmodel/10131263 (the netCDF binary for the above model expressed as absolute velocity)\
 Changes      : lose variable "vs", rename variable "xi" to "v".
+
+5. MITP08_dvp.nc\
+Paper        : doi:10.1029/2007GC001806
+Download link: https://agupubs.onlinelibrary.wiley.com/action/downloadSupplement?doi=10.1029%2F2007GC001806&file=ggge1202-sup-0002-ds01.txt.gz\
+Changes      : convert the ascii file into nc file. rename variable "dvp" to "v".
+
+6. MITP08_vp.nc\
+Paper        : doi:10.1029/2007GC001806
+Download link: https://agupubs.onlinelibrary.wiley.com/action/downloadSupplement?doi=10.1029%2F2007GC001806&file=ggge1202-sup-0002-ds01.txt.gz\
+Changes      : convert dvp into vp using ak135 reference model and create this nc file in which "v" representing "vp".
